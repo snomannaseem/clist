9fe069c5505a58d070b9b6f71048aa23d58765fd
Noman Naseem <noman.naseem@centricsource.com>
2016-11-15 23:28:53 +0500

    First DB Script

---
+++ clist_db.sql
@@ -0,0 +1,130 @@
+/*
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.7.11-4 : Database - clist_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `clist_db`;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `categories` */
+
+/*Table structure for table `categories_n_classifieds` */
+
+DROP TABLE IF EXISTS `categories_n_classifieds`;
+
+CREATE TABLE `categories_n_classifieds` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(10) unsigned NOT NULL,
+  `classified_id` int(10) unsigned NOT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `published_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categories_category_id` (`category_id`),
+  KEY `fk_categories_classifieds_classified_id` (`classified_id`),
+  CONSTRAINT `fk_categories_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `fk_categories_classifieds_classified_id` FOREIGN KEY (`classified_id`) REFERENCES `classifieds` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `categories_n_classifieds` */
+
+/*Table structure for table `classified_favorites` */
+
+DROP TABLE IF EXISTS `classified_favorites`;
+
+CREATE TABLE `classified_favorites` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `classified_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_user_id` (`user_id`),
+  KEY `fk_favorites_classifieds_classified_id` (`classified_id`),
+  CONSTRAINT `fk_favorites_classifieds_classified_id` FOREIGN KEY (`classified_id`) REFERENCES `classifieds` (`id`),
+  CONSTRAINT `fk_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `classified_favorites` */
+
+/*Table structure for table `classified_images` */
+
+DROP TABLE IF EXISTS `classified_images`;
+
+CREATE TABLE `classified_images` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `classified_images` */
+
+/*Table structure for table `classified_votes` */
+
+DROP TABLE IF EXISTS `classified_votes`;
+
+CREATE TABLE `classified_votes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `classified_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `classified_votes` */
+
+/*Table structure for table `classifieds` */
+
+DROP TABLE IF EXISTS `classifieds`;
+
+CREATE TABLE `classifieds` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `title` varchar(1024) NOT NULL,
+  `detail` text,
+  `created_on` datetime DEFAULT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  `published_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `classifieds` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` char(1) DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
